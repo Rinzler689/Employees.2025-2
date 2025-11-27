380c1c74e62b5df0485e39df81984455be696f39
Rinzler689 <kevinney10@hotmail.com>
2025-11-27 14:01:07 -0500

    photo components with Azure added

--- Employees/Employees.Backend/Program.cs
+++ Employees/Employees.Backend/Program.cs
@@ -1,4 +1,5 @@
 using Employees.Backend.Data;
+using Employees.Backend.Helpers;
 using Employees.Backend.Repositories.Implementations;
 using Employees.Backend.Repositories.Interfaces;
 using Employees.Backend.UnitsOfWork.Implementations;
@@ -57,6 +58,7 @@ builder.Services.AddDbContext<DataContext>(x =>
     }));
 
 builder.Services.AddTransient<SeedDb>();
+builder.Services.AddScoped<IFileStorage, FileStorage>();
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(x => x.TokenValidationParameters = new TokenValidationParameters
